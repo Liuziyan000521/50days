9cf2f89f16be892c5808fda265d4be635e666d96
SharkChili <510763668@qq.com>
2022-08-18 23:54:48 +0800

    10-随机笑话

---
+++ day10/index.js
@@ -0,0 +1,17 @@
+const jokeEl = document.getElementById("joke")
+const jokeBtn = document.getElementById("jokeBtn")
+
+jokeBtn.addEventListener('click', getjoke)
+
+getjoke();
+
+async function getjoke() {
+    const config = {
+        headers: {
+            Accept: 'application/json',
+        }
+    }
+    const res = await fetch('https://icanhazdadjoke.come', config)
+    const data = await res.json();
+    jokeEl.innerHTML = data.joke;
+}
